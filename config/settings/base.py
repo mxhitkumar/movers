3ea8f2e70398173ee6cd32a80a7837fa6afeef1a
mohitcareers360 <mohit.k@careers360.in>
2025-12-26 22:17:42 +0530

    add media

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,6 +56,7 @@ INSTALLED_APPS += PROJECT_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -147,3 +148,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('cms/', admin.site.urls),
     path('', include("booking.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
